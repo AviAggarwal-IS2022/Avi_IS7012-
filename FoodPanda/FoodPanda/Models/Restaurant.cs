f7c93675525b70e9da468f9bae20bc9b4ad4dafb
AviAggarwal-IS2022 <anka.2008@hotmail.com>
2022-10-05 13:17:51 -0400

    FoodPanda Final Submission

--- FoodPanda/FoodPanda/Models/Restaurant.cs
+++ FoodPanda/FoodPanda/Models/Restaurant.cs
@@ -7,16 +7,12 @@ namespace FoodPanda.Models
     {
         public int RestaurantId { get; set; }
         [DisplayName("Restaurant")]
-        [Required(ErrorMessage = "Restaurant name is required")]
-        [StringLength(100, MinimumLength = 1, ErrorMessage = ("Please enter a valid name"))]
         public string RestaurantName { get; set; }
         public int? Capacity { get; set; }
         public Cuisine? Cuisine { get; set; }
         public int? CuisineId { get; set; }
         public Dish? Dish { get; set; }
         public int? DishId { get; set; }
-        [Required(ErrorMessage = "Location is required")]
-        [StringLength(100, MinimumLength = 1, ErrorMessage = ("Please select a location"))]
         public Location? Location { get; set; }
         public int? LocationId { get; set; }
         public List<Review>? Review { get; set; }
